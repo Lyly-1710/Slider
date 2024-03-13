be69242a4adf9a927d0fb16bd71b124d9a3bb4cd
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-03-13 15:39:18 +0700

    first commit

--- slide.js
+++ slide.js
@@ -1,12 +1,11 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -18,7 +17,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i] 
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
